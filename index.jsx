7ebd473fdd675a35c897c32509ac9888557d690b
Jason Hills <jasonahills@gmail.com>
2015-04-28 20:29:55 -0600

    can move paddles (buggy)

--- index.jsx
+++ index.jsx
@@ -7,6 +7,14 @@ var PADDLE_HEIGHT = 20
 var HALF_PADDLE_HEIGHT = PADDLE_HEIGHT/2
 var PADDLE_WIDTH = 3
 var HALF_PADDLE_WIDTH = PADDLE_WIDTH/2
+var PADDLE_MOVE_SPEED = 1
+
+var keysPressed = {
+    w: false,
+    s: false,
+    i: false,
+    k: false
+}
 
 var gameState = {
     leftPaddle: {
@@ -30,15 +38,11 @@ var HelloWorld = React.createClass({
 
 
 var GameBoard = React.createClass({
-    onMouseMove: function(event) {
-        var coords = eventCoordinates(event)
-        console.log("coords", coords.x, coords.y)
-    },
 
     render: function() {
         var viewBox = "0 0 " + GAMEBOARD_WIDTH + " " + GAMEBOARD_WIDTH
         return (
-            <svg {...this.props} viewBox={viewBox} onMouseMove={this.onMouseMove}>
+            <svg {...this.props} viewBox={viewBox}>
                 <Paddle paddle={this.props.gameState.leftPaddle}/>
                 <Paddle paddle={this.props.gameState.rightPaddle}/>
             </svg>
@@ -62,17 +66,56 @@ window.setInterval(function() {
         <GameBoard width="50%" gameState={gameState} >
         </GameBoard>, document.getElementById("react-here")
     )
-}, 20)
+}, 100)
 
 //KEYBOARD STUFF
-var listener = new window.keypress.Listener();
+var W_KEYCODE = 87
+var S_KEYCODE = 83
+var I_KEYCODE = 73
+var K_KEYCODE = 75
+
+document.addEventListener("keydown", function(event) {
+    var code = event.keyCode
+    if (code == W_KEYCODE) {keysPressed.w = true}
+    if (code == S_KEYCODE) {keysPressed.s = true}
+    if (code == I_KEYCODE) {keysPressed.i = true}
+    if (code == K_KEYCODE) {keysPressed.k = true}
+})
+
+document.addEventListener("keyup", function(event) {
+    var code = event.keycode
+    if (code == W_KEYCODE) {keysPressed.w = false}
+    if (code == S_KEYCODE) {keysPressed.s = false}
+    if (code == I_KEYCODE) {keysPressed.i = false}
+    if (code == K_KEYCODE) {keysPressed.k = false}
+        console.log("keyup")
+})
+
+
+window.setInterval(function() {
+    if (keysPressed.w && keysPressed.s) {}
+    else if (keysPressed.w) {movePaddleUp(gameState.leftPaddle)}
+    else if (keysPressed.s) {movePaddleDown(gameState.leftPaddle)}
+
+    if (keysPressed.i && keysPressed.k) {}
+    else if (keysPressed.i) {movePaddleUp(gameState.rightPaddle)}
+    else if (keysPressed.k) {movePaddleDown(gameState.rightPaddle)}
+
+}, 30)
 
 
 
-function eventCoordinates(event) {
-    console.log("pageY", event.pageY, event.target.offsetTop)
-    return {
-        x: event.pageX - event.target.offsetLeft,
-        y: event.pageY - event.target.offsetTop
-    }
+
+// GAME STATE MANIPULATION
+
+function movePaddleDown(paddle) {
+    movePaddle(paddle, PADDLE_MOVE_SPEED)
+}
+
+function movePaddleUp(paddle) {
+    movePaddle(paddle, -1*PADDLE_MOVE_SPEED)
+}
+
+function movePaddle(paddle, amount) {
+    paddle.y = paddle.y + amount
 }
